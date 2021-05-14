f203bcc6ee4299dfcb178b9b54cac1e2ea8fc968
JAUME PUIG <jaume.puig@skyscanner.net>
2021-05-14 23:55:34 +0200

    Add upcoming events

--- Views/Upcoming.js
+++ Views/Upcoming.js
@@ -1,12 +1,22 @@
 import * as React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView } from "react-native";
+import { StatusBar } from "expo-status-bar";
 
 export default function Upcoming() {
+  const renderEvents = () => {
+    return [1, 2, 3, 4, 5, 6, 7].map((event) => (
+      <View style={styles.event}>
+        <Text style={styles.title}>enFIBat!</Text>
+        <Text style={styles.title}>enFIBat!</Text>
+        <Text style={styles.title}>enFIBat!</Text>
+      </View>
+    ));
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>enFIBat!</Text>
       <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>{renderEvents()}</ScrollView>
     </View>
   );
 }
@@ -16,10 +26,21 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 20,
   },
   title: {
+    color: "white",
     fontSize: 20,
     fontWeight: "bold",
+  },
+  event: {
+    backgroundColor: "black",
+    width: "100%",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  scrollView: {
+    width: "100%",
   },
 });
