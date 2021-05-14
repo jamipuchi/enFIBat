59f5d1a5f80d574452ac9a4c82459ee7aeacfecf
JAUME PUIG <jaume.puig@skyscanner.net>
2021-05-14 23:38:13 +0200

    Add bottom navigation bar

---
+++ Views/SubjectForm.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+
+export default function SubjectForm() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>enFIBat!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
