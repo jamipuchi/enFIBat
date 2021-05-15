5ac61d93eaa5fa403babb142aeec2355ddf80eb7
JAUME PUIG <jaume.puig@skyscanner.net>
2021-05-15 09:13:45 +0200

    Finish upcoming

--- Views/Upcoming.js
+++ Views/Upcoming.js
@@ -1,16 +1,49 @@
 import * as React from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import mockJsonData from "../mockdata/upcoming";
 
 export default function Upcoming() {
+  const eventTypes = [
+    { name: "academic", color: ["#ffc72c", "#ebec00"] },
+    { name: "event", color: "#e56a54" },
+    { name: "social", color: "#6da04b" },
+    { name: "talks", color: "#002f56" },
+  ];
+
+  const getColorForEvent = (event) => {
+    if (event.type === "academic") {
+      return event.description.type === "delivery" ? "#ffc72c" : "#ebec00";
+    }
+    if (event.type === "event") {
+      return "#e56a54";
+    }
+    if (event.type === "social") {
+      return "#6da04b";
+    }
+    if (event.type === "talks") {
+      return "#2f9fd0";
+    }
+  };
+
   const renderEvents = () => {
-    return [1, 2, 3, 4, 5, 6, 7].map((event) => (
-      <View style={styles.event}>
-        <Text style={styles.title}>enFIBat!</Text>
-        <Text style={styles.title}>enFIBat!</Text>
-        <Text style={styles.title}>enFIBat!</Text>
-      </View>
-    ));
+    return mockJsonData.map((event) => {
+      const backgroundColor = getColorForEvent(event);
+      console.log(backgroundColor);
+      return (
+        <View key={event.id} style={styles.event}>
+          <Text style={styles.date}>{event.date[0]}</Text>
+          <Text style={styles.hour}>{event.date[1]}</Text>
+          <View style={{ ...styles.title, backgroundColor: backgroundColor }}>
+            <Text style={styles.titleText}>
+              {event.description.summary
+                ? event.description.summary
+                : event.description}
+            </Text>
+          </View>
+        </View>
+      );
+    });
   };
 
   return (
@@ -29,12 +62,15 @@ const styles = StyleSheet.create({
     paddingTop: 20,
   },
   title: {
-    color: "white",
+    flex: 1,
     fontSize: 20,
     fontWeight: "bold",
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "red",
   },
   event: {
-    backgroundColor: "black",
+    flexDirection: "row",
     width: "100%",
     padding: 10,
     marginBottom: 10,
@@ -43,4 +79,7 @@ const styles = StyleSheet.create({
   scrollView: {
     width: "100%",
   },
+  date: { width: "20%" },
+  hour: { width: "10%" },
+  titleText: { color: "black" },
 });
